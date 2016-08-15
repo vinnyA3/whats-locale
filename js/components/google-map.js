00aebb7a2bfae0cee821eeabe69bfd892519ce7a
vinnya3 <vinaceto@yahoo.com>
2016-08-15 11:56:56 -0400

    fix: Fix React import syntax bug

--- js/components/google-map.js
+++ js/components/google-map.js
@@ -1,5 +1,4 @@
-import { Component } from "react";
-import ReactDOM from "react-dom";
+import React,{ Component } from "react";
 
 export default class GoogleMap extends Component {
 
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+// Import App
+import App from './components/app.js';
+// Render Application
+ReactDOM.render(<App />, document.getElementById('app'));
